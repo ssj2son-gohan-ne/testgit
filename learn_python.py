b6fdf2c55c8d6618a0c58fee9e260de8fe62837a
ssj2son-gohan-ne <58134335+ssj2son-gohan-ne@users.noreply.github.com>
2021-04-07 20:18:42 +0200

    Create learn_python.py

---
+++ learn_python.py
@@ -0,0 +1,32 @@
+# learn python: https://www.learnpython.org/en/Classes_and_Objects
+
+# define the Vehicle class
+class Vehicle:
+    name = ""
+    kind = "car"
+    color = ""
+    value = 100.00
+    def description(self):
+        desc_str = "%s is a %s %s worth $%.2f." % (self.name, self.color, self.kind, self.value)
+        return desc_str
+
+# your code goes here
+car1 = Vehicle()
+car1.name = "fer"
+car1.kind = "convertible"
+car1.color = "red"
+car1.value = 60.000
+
+car2 = Vehicle()
+car2.name = "Jump"
+car2.kind = "van"
+car2.color = "blue"
+car2.value = 10.000
+
+# test code
+print(car1.description())
+print(car2.description())
+
+
+    fer is a red convertible worth $60.00.
+    Jump is a blue van worth $10.00.
